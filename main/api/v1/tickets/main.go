c5e23bd9b5845b81ee9227f20857724bd92401eb
krombi <shuvaev.alexey@yandex.ru>
2020-11-09 00:07:13 +0300

    adding ticket handler

--- main/api/v1/tickets/main.go
+++ main/api/v1/tickets/main.go
@@ -1,10 +1,13 @@
 package tickets
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type data struct {
@@ -25,6 +28,20 @@ func GetList(w http.ResponseWriter, r *http.Request) {
 
 	_, err := fmt.Fprintf(w, "Hello, my name is %s %s!", storage.Name, storage.LastName)
 	if err != nil {
+		return
+	}
+
+}
+
+func Read(w http.ResponseWriter, r *http.Request) {
+
+	db, err := sql.Open("mysql", "goexp:F76MWx3Px2@/exp_golang")
+	if err != nil {
+		return
+	}
+
+	_, createErr := db.Query("CREATE TABLE `tickets` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `url` varchar(50) NOT NULL)")
+	if createErr != nil {
 		return
 	}
 
