d11d248be9c14265b65731d0bb86d6797f6fec13
krombi <shuvaev.alexey@yandex.ru>
2020-11-08 10:03:25 +0300

    upd

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"./api/v1/tickets"
@@ -8,14 +9,23 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func methodNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	if _, err := fmt.Fprintln(w, "123"); err != nil {
+		return
+	}
+}
+
 func main() {
 
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(methodNotFound)
+
 	V1 := r.PathPrefix("/v1").Subrouter()
 
 	V1.HandleFunc("/tickets", tickets.GetList).Methods("GET", "POST")
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		return
 	}
 
